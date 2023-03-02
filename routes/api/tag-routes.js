1e9f6c142abb6c2c2f56912ff96bd87e698acd53
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-03-02 10:13:55 -0700

    error on npm run seed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Tag, Product, ProductTag } = require("../../models");
+const { Tag, Product } = require("../../models");
 
 // The `/api/tags` endpoint
 
@@ -51,7 +51,7 @@ router.put("/:id", async (req, res) => {
         id: req.params.id,
       },
     });
-    if (!tagData[0]) {
+    if (!tagData) {
       res.status(404).json({ message: "No tag found with this id!" });
       return;
     }
